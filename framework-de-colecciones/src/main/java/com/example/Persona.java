6ba70ed95b0330f9c990567ac679ccf4f42cae83
ninoskagrillo25-sys <ninoskagrillo25@gmail.com>
2026-07-01 18:03:12 +0200

    antes de crear la rama map_interface

--- framework-de-colecciones/src/main/java/com/example/Persona.java
+++ framework-de-colecciones/src/main/java/com/example/Persona.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import lombok.Builder;
 
@@ -18,37 +19,24 @@ public record Persona(String nombre, String apellido1, String apellido2, Genero 
 		// el primer apellido, que decida el segundo Apellido, y si tambien por el
 		// segundo
 		// Apellido fueran iguales, pues que decida el nombre
+
 		// La variable siguiente, va a almacenar el resultado de la comparacion
 		// del primer apellido de dos personas, una es la persona correspondiente al
 		// objeto
 		// Persona actual, al cual se accede mediante la palabra reservada this, y la
 		// otra
 		// persona es la que se recibe como parametro del metodo compareTo
-
 		int cmpPrimerApellido = this.apellido1.compareTo(persona.apellido1());
-		
-		int cmpPrimerApellido1 = this.apellido1.compareTo(persona.apellido1()); 
 		int cmpSegundoApellido = this.apellido2.compareTo(persona.apellido2());
 		int cmpNombre = this.nombre.compareTo(persona.nombre());
 
-		return cmpPrimerApellido1 != 0 ? 
-		cmpPrimerApellido1 : 
-		cmpSegundoApellido != 0 ? 
-		cmpSegundoApellido :
-		cmpNombre;
-		
-		// Recordando cual es la sintaxis del operador ternario
+		return cmpPrimerApellido != 0 ? cmpPrimerApellido : cmpSegundoApellido != 0 ? cmpSegundoApellido : cmpNombre;
 
-		/*
-		int x = 4;
-		int y = 5;
+	}
 
-		String resultado;
+	public long edad() {
 
-		resultado = (x != y && x > 2 || y >= 5) ? "Si, se cumple" : "No, no se cumple";
-		*/
-		
-		
+		return ChronoUnit.YEARS.between(fechaNacimiento, LocalDate.now());
 
 	}
 
